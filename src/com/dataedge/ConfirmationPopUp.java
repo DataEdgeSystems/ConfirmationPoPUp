b87bca30fcc5822f1c060ca4966810fb2a5f82ea
VikRamShaRma <VikRamShaRma@DESKTOP-LAKEOP7>
2016-08-02 22:55:13 +0530

    Confirmation popup demo

---
+++ src/com/dataedge/ConfirmationPopUp.java
@@ -0,0 +1,21 @@
+package com.dataedge;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ConfirmationPopUp {
+	WebDriver driver=new FirefoxDriver();
+	@Test
+	public void ExampleForConfirmBox() throws InterruptedException
+	{
+		driver.manage().window().maximize();
+		driver.get("file:///C:/Users/VikRamShaRma/Desktop/selenium/confirmation.html");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@onclick='confirmFunction()']")).click();
+		Alert alert=driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.dismiss();
+	}
+}
